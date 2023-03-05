42f772b5d814ff5c20c01acc538d5b7fa5bdc9c5
Raul <iraulcv@gmail.com>
2023-03-05 01:23:56 -0500

    New Post Component

--- src/components/posts/NewPost.tsx
+++ src/components/posts/NewPost.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useAddPostMutation } from '../../services/golangAPI'
 
 function newPost() {
+  const [content, setContent] = useState('')
+  const [isNewPost, setIsNewPost] = useState(false)
+  const [addPost, { isLoading, isError }] = useAddPostMutation()
+
+  const onHandleSubmmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    addPost({ content })
+    setContent('')
+    setIsNewPost(false)
+  }
   return (
     <div>
-      <button>Add new Post</button>
+      {
+        isNewPost ?
+          isLoading ?
+            <div>Adding a new Post</div>
+            : (
+              <div>
+                <form onSubmit={onHandleSubmmit}>
+                  <textarea
+                    name="content"
+                    id="contentId"
+                    rows={3}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
+                  <button>Save</button>
+                </form>
+                <button onClick={() => setIsNewPost(!isNewPost)}>Cancel</button>
+              </div>
+            )
+          :
+          <button onClick={() => setIsNewPost(!isNewPost)}>Add new Post</button>
+      }
     </div>
   )
 }
--- src/services/golangAPI.ts
+++ src/services/golangAPI.ts
@@ -1,15 +1,35 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+interface Post {
+  id: string
+  content: string
+}
+type PostsResponse = Post[]
 
 export const golangApi = createApi({
   reducerPath: 'golangApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3020' }),
+  tagTypes: ['Posts'],
   endpoints: (build) => ({
     getPosts: build.query({
       query: ({ page }) => `/posts?page=${page}`
+    }),
+    getPost: build.query({
+      query: ({ id }) => `/posts/${id}`
+    }),
+    addPost: build.mutation<Post, Partial<Post>>({
+      query: (body) => ({
+        url: '/posts',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['Posts'],
     }),
   }),
 })
 
 export const {
-  useGetPostsQuery
+  useGetPostsQuery,
+  useGetPostQuery,
+  useAddPostMutation,
+  
 } = golangApi
